e0c2beaa6868db536105b47eb0de343d4df3f614
Petr Borul <psborul@gmail.com>
2020-05-27 12:14:07 +0300

    add stable WS connection

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import VueSocketIO from 'vue-socket.io';
 import store from '../store';
+import io from 'socket.io-client';
+
+const socketInstance = io('/', {
+  transports: ['websocket'],
+  rejectUnauthorized: false,
+});
 
 export default ({ store }) => {
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: '/',
+    connection: socketInstance,
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
       mutationPrefix: 'SOCKET_',
-  },
+    },
   }));
 };
